f57141079ef4f0d7b0d4d8193d3b75d2b93d563d
Pranas Ziaukas <pranas@balena.io>
2021-06-16 17:03:47 +0300

    Add actual value counter
    Change-type: minor
    Signed-off-by: Pranas Ziaukas <pranas@balena.io>

--- src/gsheets_cell_counter/__init__.py
+++ src/gsheets_cell_counter/__init__.py
@@ -5,7 +5,8 @@ __token__ = 'token.pickle'
 __version__ = '0.1.0'
 
 from .authentication import get_service
+from .usage import Index
 from .cli import get_cell_counts
 
 
-__all__ = [__credentials__, __token__, __version__, get_service, get_cell_counts]
+__all__ = [__credentials__, __token__, __version__, get_service, get_cell_counts, Index]
